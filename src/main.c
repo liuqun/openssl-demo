0f79465eecbb2d723d01f2c4e01cf48988cbf425
Liu Qun (liuqun) <517067180@qq.com>
2018-04-17 09:35:22 +0800

    main.c: Use unsigned char as default data type
    使用 unsigned char 比 uint8_t 可移植性更好

--- src/main.c
+++ src/main.c
@@ -3,17 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#if defined(HAVE_CONFIG_H)
-#include "config.h"
-#endif
-
-#if defined(HAVE_STDINT_H)
-#include <stdint.h>
-#else
-typedef unsigned char uint8_t;
-#endif
-
 #include "openssl/err.h"
 #include "openssl/evp.h"
 
@@ -24,18 +13,18 @@ static const EVP_CIPHER *(*EVP_sm4_ecb)()=EVP_aes_128_ecb;
 #endif
 
 typedef struct {
-	const void *in_data;
+	const unsigned char *in_data;
 	size_t in_data_len;
 	int in_data_is_already_padded;
-	const uint8_t *in_ivec;
-	const void *in_key;
+	const unsigned char *in_ivec;
+	const unsigned char *in_key;
 	size_t in_key_len;
 } test_case_t;
 
 
 void test_encrypt_with_cipher(const test_case_t *in, const EVP_CIPHER *cipher)
 {
-	uint8_t *out_buf = NULL;
+	unsigned char *out_buf = NULL;
 	int out_len;
 	int out_padding_len;
 	EVP_CIPHER_CTX *ctx;
@@ -59,7 +48,7 @@ void test_encrypt_with_cipher(const test_case_t *in, const EVP_CIPHER *cipher)
 		EVP_CIPHER_CTX_set_padding(ctx, 0);
 	}
 
-	out_buf = (uint8_t *) malloc(((in->in_data_len>>4)+1) << 4);
+	out_buf = (unsigned char *) malloc(((in->in_data_len>>4)+1) << 4);
 	out_len = 0;
 	EVP_EncryptUpdate(ctx, out_buf, &out_len, in->in_data, in->in_data_len);
 	if (1)
@@ -98,23 +87,23 @@ void main()
 {
 	int have_sm4 = (OPENSSL_VERSION_NUMBER >= 0x10101001L);
 	int have_aes = 1;
-	const uint8_t data[]=
+	const unsigned char data[]=
 	{
 		0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
 		0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10,
 	};
-	uint8_t ivec[EVP_MAX_IV_LENGTH]; ///< IV 向量
-	const uint8_t key1[16] = ///< key_data, 密钥内容, 至少16字节
+	unsigned char ivec[EVP_MAX_IV_LENGTH]; ///< IV 向量
+	const unsigned char key1[16] = ///< key_data, 密钥内容, 至少16字节
 	{
 		0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
 		0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10,
 	};
 	test_case_t tc;
 
-	tc.in_data = (void *) data;
+	tc.in_data = data;
 	tc.in_data_len = sizeof(data);
 	tc.in_data_is_already_padded = (tc.in_data_len % 16)==0; // Hard coded 16 as the cipher's block size
-	tc.in_key = (void *) key1;
+	tc.in_key = key1;
 	tc.in_key_len = sizeof(key1);
 	memset(ivec, 0x00, EVP_MAX_IV_LENGTH);
 	tc.in_ivec = ivec;
