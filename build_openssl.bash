2fb55f01f24de98df6f2e97c82bf08eaa49de693
LiuQun <517067180@qq.com>
2019-06-06 21:10:26 +0800

    更正build_openssl末尾的echo信息

--- build_openssl.bash
+++ build_openssl.bash
@@ -4,4 +4,4 @@ PREFIX=/usr/local
 build_dir=openssl
 cd $build_dir
 ./config enable-sm2 enable-sm3 enable-sm4 --prefix="$PREFIX" && make \
-	&& echo "Build OK! type: cd sudo make -C openssl install"
+	&& echo "Build OK! type: sudo make -C openssl install"
